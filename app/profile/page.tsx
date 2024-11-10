b37d17ff6339cd77a5f0067cc6bb0a9b8cfc1328
savsch <hmm>
2024-11-10 22:56:11 +0530

    Added navbar to profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ProfilePage from "@/components/ProfilePage";
+import { Navbar } from "@/components/transparent-navbar-with-logo";
 
 export default function UserProfile() {
   return (
   <div className="min-h-screen bg-[#191919] flex flex-col items-center">
-    <ProfilePage />
+    <Navbar />
+    <main className="mt-12 sm:mt-9">
+      <ProfilePage/>
+    </main>
   </div>
   );
 }
