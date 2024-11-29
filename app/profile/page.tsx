0fd4803e4e4c8189365c537503a50f34284764a2
savsch <hmm>
2024-11-29 09:28:10 +0530

    Removed Redundant Navbar Import

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ProfilePage from '@/app/layout/ProfilePage';
-import { Navbar } from '@/app/layout/transparent-navbar-with-logo';
 
 export default function UserProfile() {
   return (
     <div className="flex min-h-screen flex-col items-center bg-[#191919]">
-      <Navbar />
       <main className="mt-12 sm:mt-9">
         <ProfilePage />
       </main>
