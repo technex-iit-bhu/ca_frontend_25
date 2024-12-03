96556d865937cecf0e1985d05396c9bdaf2cc094
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-12-03 12:18:10 +0530

    Update base url
    Signed-off-by: Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,6 +1,6 @@
 import { Task, User } from '../layout/dashboard/dashboard';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function getProfileDetails(token: string) {
   const res = await fetch(`${BASE_URL}/user/profile`, {
