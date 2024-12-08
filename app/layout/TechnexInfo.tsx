3241621b3baf55eae67d6fca1406acbd7db4e358
Abhinav Pandey <abhinavpandey1230@gmail.com>
2024-12-08 22:16:15 +0530

    Align RedLines properly
    Signed-off-by: Abhinav Pandey <abhinavpandey1230@gmail.com>

--- app/layout/TechnexInfo.tsx
+++ app/layout/TechnexInfo.tsx
@@ -6,17 +6,26 @@ export default function TechnexInfo() {
       id="first-section"
       className="flex h-screen flex-col items-start justify-center space-y-6 px-6 pt-8 text-left sm:px-12 md:px-24 md:pt-0"
     >
-      <Image
+      {/* <Image
         src="/homebg.png"
         alt="Home Background"
         layout="fill"
         objectFit="cover"
         className="z-[0]"
-      />
-      <h1 className="text-3xl font-normal sm:text-4xl md:text-5xl z-[0] font-signika">
+      /> */}
+      <div className="absolute inset-0 z-0">
+        <Image
+          src="/homebg.png"
+          alt="Home Background"
+          layout="fill"
+          objectFit="cover"
+          className="z-[-1] w-full h-full"
+        />
+      </div>
+      <h1 className="font-signika z-[0] text-3xl font-normal sm:text-4xl md:text-5xl">
         <span className="text-red-600">Tech</span> <span className="text-white">Trek Pioneer</span>
       </h1>
-      <p className="max-w-xl rounded-[20px] bg-black bg-opacity-80 p-4 text-lg sm:text-xl md:text-2xl z-[0]">
+      <p className="z-[0] max-w-xl rounded-[20px] bg-black bg-opacity-80 p-4 text-lg sm:text-xl md:text-2xl">
         <span className="text-2xl text-white sm:text-3xl">TECHNEX&apos;24, IIT BHU</span> is one of
         the largest and oldest college fests in India. Embodying the true spirit of youth, Technex
         provides a platform for{' '}
@@ -32,7 +41,7 @@ export default function TechnexInfo() {
 
       <Button
         variant="outline"
-        className="w-48 rounded-full bg-customRed text-white hover:bg-gray-400 z-[0]"
+        className="z-[0] w-48 rounded-full bg-customRed text-white hover:bg-gray-400"
       >
         Register
       </Button>
