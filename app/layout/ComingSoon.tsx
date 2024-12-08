f3fb49edf6ee445d033a8fe4fbc559dfc4e3b563
Abhinav Pandey <abhinavpandey1230@gmail.com>
2024-12-08 20:37:06 +0530

    Add Incentives bg
    Signed-off-by: Abhinav Pandey <abhinavpandey1230@gmail.com>

--- app/layout/ComingSoon.tsx
+++ app/layout/ComingSoon.tsx
@@ -1,6 +1,6 @@
 export default function ComingSoon() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
+    <div className="flex min-h-screen items-center justify-center">
       <h1 className="animate-bounce text-6xl font-bold text-white">Coming Soon!!</h1>
     </div>
   );
--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,5 +1,11 @@
 import ComingSoon from '../layout/ComingSoon';
+import Image from 'next/image';
 
 export default function LeaderBoard() {
-  return <ComingSoon />;
+  return (
+    <div className="relative bg-cover bg-center">
+    <Image src="/dashbg.png" alt="Background" layout="fill" objectFit="cover" />
+    <ComingSoon />
+  </div>
+  )
 }
