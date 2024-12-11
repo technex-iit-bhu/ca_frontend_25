b1ecb9ae036b54c5996d71ceafa54842952c7723
Abhinav Pandey <abhinavpandey1230@gmail.com>
2024-12-11 19:48:18 +0530

    mkc iski
    Signed-off-by: Abhinav Pandey <abhinavpandey1230@gmail.com>

--- app/layout/WhyCA.tsx
+++ app/layout/WhyCA.tsx
@@ -8,7 +8,7 @@ export default function WhyCA() {
       <HeadingTexts whiteText="Why" redText="CA?" align="center" />
       <div className="my-10 max-w-max rounded-[20px] bg-black bg-opacity-80 p-4">
         <p className="mt-4 text-white sm:text-lg md:text-xl">
-          The Campus Ambassador Program for Technex&apos;23 offers an exciting chance to become an
+          The Campus Ambassador Program for Technex&apos;24 offers an exciting chance to become an
           essential part of the organizing team behind India&apos;s premier technical and innovation
           fest. As a Campus Ambassador,{' '}
           <span className="text-customRed">
@@ -30,7 +30,7 @@ export default function WhyCA() {
           <span className="text-customRed">
             organize workshops and events, gaining hands-on experience in event planning.
           </span>{' '}
-          You&apos;ll be the face of Technex&apos;23 in your college, serving as a source of
+          You&apos;ll be the face of Technex&apos;25 in your college, serving as a source of
           information, motivation, and a connector. This program is an incredible opportunity to{' '}
           <span className="text-customRed">grow both personally and professionally</span> while
           playing a pivotal role in promoting technology and innovation at your college. Seize this
