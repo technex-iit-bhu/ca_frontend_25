a5fe161f50d9953e517b4bb79876029b5b2fcb4a
Abhinav Pandey <abhinavpandey1230@gmail.com>
2024-12-11 17:37:44 +0530

    Update dashbg
    Signed-off-by: Abhinav Pandey <abhinavpandey1230@gmail.com>

--- app/layout/ComingSoon.tsx
+++ app/layout/ComingSoon.tsx
@@ -1,6 +1,6 @@
 export default function ComingSoon() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-[url('/dashbg.png')] bg-cover bg-center">
       <h1 className="animate-bounce text-6xl font-bold text-white">Coming Soon!!</h1>
     </div>
   );
